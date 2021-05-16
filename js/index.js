c502960a012b0d202bd3ec56c42c136f64346c4b
yancleiton7 <74550820+yancleiton7@users.noreply.github.com>
2021-05-15 23:53:00 -0300

    historia

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function capturar(nome, intro, convite,  some, aparece){
     }
     else{
     
-    muda_pagina(some, aparece);
+    muda_pagina_flex(some, aparece);
     document.getElementById(intro).innerHTML = capturando;
     document.getElementById(convite).innerHTML = capturando;
     }
@@ -33,6 +33,19 @@ function show_article(id){
     
 }
 
+function show_flex(id){
+    
+    var infos = document.getElementById(id);
+   
+    if (infos.style.display === "none") {
+        infos.style.display = "flex";
+      } 
+    else {
+        infos.style.display = "none";
+      }
+    
+}
+
 
 function muda_pagina(some, aparece){
     show_article(some);
@@ -42,9 +55,28 @@ function muda_pagina(some, aparece){
     aparece = '';
     
 }
-function play_audio() {
-    document.getElementById('musica').play();
+
+function muda_pagina_flex(some, aparece){
+    show_flex(some);
+    show_flex(aparece);
+    
+    some = '';
+    aparece = '';
+    
 }
+
+function play_audio(song) {
+    musica = document.getElementById(song);
+    musica.pause();
+}
+
+function pause_audio(song) {
+    musica = document.getElementById(song);
+    musica.play();
+
+}
+
+
 
 function redirect (){
     window.location.href = "http://www.encurtador.com.br/gxEQ6";
@@ -54,12 +86,78 @@ function redirect (){
 
 
 function typeWriter(elemento) {
-    const texto = document.getElementById(elemento)
+   
+    const texto = document.getElementById(elemento);
     const textoArray = texto.innerHTML.split('');
     texto.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => texto.innerHTML += letra, 75 * i);
+      setTimeout(() => texto.innerHTML += letra, 30 * i);
     });
+    
 
 }
 
+    var atual = "vazio";
+    var prox = "prologo";
+
+function muda_texto() {
+
+    
+    if (atual === "vazio") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        typeWriter(prox);
+        atual = "prologo";
+        prox = "p1";
+    }
+    
+    else if (atual === "prologo") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        typeWriter(prox);
+        atual = "p1";
+        prox = "p2";
+    }
+
+    else if (atual === "p1") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        typeWriter(prox);
+        atual = "p2";
+        prox = "p3";
+    }
+
+    else if (atual === "p2") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        document.getElementById('yan').style.display = "flex";
+        document.getElementById('iasmin').style.display = "none";
+        typeWriter(prox);
+        atual = "p3";
+        prox = "p4";
+    }
+    else if (atual === "p3") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        typeWriter(prox);
+        atual = "p4";
+        prox = "p5";
+    }
+    else if (atual === "p4") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        document.getElementById('iasmin').style.display = "flex";
+        typeWriter(prox);
+        atual = "p5";
+        prox = "p6";
+    }
+    else if (atual === "p5") {
+        document.getElementById(atual).style.display = "none";
+        document.getElementById(prox).style.display = "flex";
+        typeWriter(prox);
+        atual = "p6";
+    }
+    else if (atual === "p6") {
+        muda_pagina("historia", "intro_padrinho")
+    }
+}
